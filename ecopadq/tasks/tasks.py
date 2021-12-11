22c6ee3c4649bdddd9d478a935b6ca20642dc8fa
Markus Müller <markus.mueller.1.g@googlemail.com>
2021-12-11 13:06:19 -0700

    test

--- ecopadq/tasks/tasks.py
+++ ecopadq/tasks/tasks.py
@@ -33,7 +33,7 @@ def test(input_a,input_b):
     task_id = str(test.request.id)
     client=SSHClient()
     client.load_system_host_keys()
-    client.set_missing_host_key_policy(AutoAddPolicy)
+    client.set_missing_host_key_policy(AutoAddPolicy())
     client.connect('local_fortran_example',username='mm')
     ssh_cmd = "./test {0} {1}".format(input_a, input_b)
     stdin, stdout, stderr = client.exec_command(ssh_cmd)
